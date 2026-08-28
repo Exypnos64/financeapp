23ee40b725d9eec9bcc276c10a3a3f57de3d3d1d
Nathan Kiehl <nathankiehl@gmail.com>
2026-08-28 11:30:26 -0500

    update(DB): Provide proper categories and merchants to the transaction seeds.

--- MSSQL/Script.PostDeployment.sql
+++ MSSQL/Script.PostDeployment.sql
@@ -252,16 +252,13 @@ BEGIN
   SET IDENTITY_INSERT dbo.Account OFF;
 END
 
-DECLARE @Col AS INT;
-SELECT @Col = Id FROM Category WHERE GroupId = 1 AND DefaultId = 1;
-
 IF NOT EXISTS (SELECT 1 FROM dbo.LedgerEntry)
 BEGIN
   INSERT INTO dbo.LedgerEntry (AccountId, MerchantId, GroupId, CategoryId, Amount, UserDate, LastModifiedUtc) VALUES
-      (1, 1, 1, @Col, 505.00,   '2025-02-21 00:00:00 -06:00', @CurUtc)
-    , (1, 1, 1, @Col, -5.99,    '2025-03-21 00:00:00 -05:00', @CurUtc)
-    , (1, 1, 1, @Col, -38.73,   '2025-03-26 00:00:00 -05:00', @CurUtc)
-    , (1, 1, 1, @Col, -173.22,  '2025-03-31 00:00:00 -05:00', @CurUtc)
-    , (1, 1, 1, @Col, -65,      '2025-04-06 00:00:00 -05:00', @CurUtc)
-    , (1, 1, 1, @Col, 500,      '2025-04-13 00:00:00 -05:00', @CurUtc);
+      (1, 3, 1, (SELECT Id FROM Category WHERE GroupId = 1 AND DefaultId = 49), 505.00,   '2025-02-21 00:00:00 -06:00', @CurUtc)
+    , (1, 6, 1, (SELECT Id FROM Category WHERE GroupId = 1 AND DefaultId = 13), -5.99,    '2025-03-21 00:00:00 -05:00', @CurUtc)
+    , (1, 4, 1, (SELECT Id FROM Category WHERE GroupId = 1 AND DefaultId = 25), -38.73,   '2025-03-26 00:00:00 -05:00', @CurUtc)
+    , (1, 7, 1, (SELECT Id FROM Category WHERE GroupId = 1 AND DefaultId = 12), -173.22,  '2025-03-31 00:00:00 -05:00', @CurUtc)
+    , (1, 7, 1, (SELECT Id FROM Category WHERE GroupId = 1 AND DefaultId = 32), -65,      '2025-04-06 00:00:00 -05:00', @CurUtc)
+    , (1, 3, 1, (SELECT Id FROM Category WHERE GroupId = 1 AND DefaultId = 49), 500,      '2025-04-13 00:00:00 -05:00', @CurUtc);
 END
