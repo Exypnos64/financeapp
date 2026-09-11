31d077558a5b895da152f5f48ae970a55f5d852d
Nathan Kiehl <nathankiehl@gmail.com>
2026-09-11 10:30:08 -0500

    feat(SvelteKit): Add the transaction entry form.

--- SvelteKit/src/lib/types.ts
+++ SvelteKit/src/lib/types.ts
@@ -1,2 +1,5 @@
 export type Account = { id: number, typeId: number, name: string, startDateUtc: string, isActive: boolean, startBalance: number, lastModifiedUtc: string };
+export type CategorySet = { id: number, name: string };
+export type Category = { id: number, name: string, set: CategorySet };
+export type Merchant = { id: number, name: string };
 export type Transaction = { id: number, account: string, merchant: string, category: string, amount: number, cashBack: number | null, userDate: string };
